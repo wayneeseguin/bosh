8d305c2dd703b3a6938a3f502003800d8a867ae0
Wayne E. Seguin <wayneeseguin@gmail.com>
2015-03-19 13:09:25 -0700

    Code cleanup, merging version tests.

--- stemcell_builder/stages/base_centos/apply.sh
+++ stemcell_builder/stages/base_centos/apply.sh
@@ -3,27 +3,45 @@
 set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
+
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/etc/settings.bash
 
 mkdir -p $chroot/var/lib/rpm
 rpm --root $chroot --initdb
+
+# Setting locale
 case "${stemcell_operating_system_version}" in
-  "6.5")
-    centos_release_package_url="http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-6.el6.centos.12.2.x86_64.rpm"
-    epel_package_url="http://ftp.osuosl.org/pub/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm"
+  (6|6.*) 
+    locale_file=/etc/sysconfig/i18n 
+    releaseVersion=6 
     ;;
-  "7")
-    centos_release_package_url="http://mirror.centos.org/centos/7/os/x86_64/Packages/centos-release-7-0.1406.el7.centos.2.3.x86_64.rpm"
-    epel_package_url="http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm"
+  (7|7.*) 
+    locale_file=/etc/locale.conf 
+    releaseVersion=7 
     ;;
-  *)
-    echo "Unknown centos version: ${stemcell_operating_system_version}"
+  (*)
+    echo "Unknown or unset CentOS release version: '${stemcell_operating_system_version}'"
     exit 1
     ;;
 esac
+releaseURL="http://mirror.centos.org/centos/${releaseVersion}/os/x86_64/Packages/"
+epelURL="https://dl.fedoraproject.org/pub/epel/${releaseVersion}/x86_64/e/"
 
-rpm --root $chroot --force --nodeps --install ${centos_release_package_url}
+# Determine the centos version release rpm url:
+releaseRPM=$(curl -s ${releaseURL} | grep centos-release- | sed -e 's/^.*href=\"//' -e 's/".*$//')
+if [[ -z ${releaseRPM} ]]
+then
+  echo "ERROR: releaseRPM not found at ${releaseURL}"
+  exit 1
+fi
+# Determine the epel version release rpm url:
+epelRPM=$(curl -s ${epelURL} | grep epel-release- | sed -e 's/^.*href=\"//' -e 's/".*$//')
+if [[ -z ${releaseRPM} ]]
+then echo "ERROR: epelRPM not found at ${epelURL}" >&2 && exit 1
+fi
+
+rpm --root $chroot --force --nodeps --install "${releaseURL}${releaseRPM}"
 
 cp /etc/resolv.conf $chroot/etc/resolv.conf
 
@@ -31,15 +49,14 @@ dd if=/dev/urandom of=$chroot/var/lib/random-seed bs=512 count=1
 
 unshare -m $SHELL <<INSTALL_YUM
   set -x
-
   mkdir -p /etc/pki
   mount --no-mtab --bind $chroot/etc/pki /etc/pki
   yum --installroot=$chroot -c /bosh/stemcell_builder/etc/custom_yum.conf --assumeyes install yum
 INSTALL_YUM
 
 run_in_chroot $chroot "
-rpm --force --nodeps --install ${centos_release_package_url}
-rpm --force --nodeps --install ${epel_package_url}
+rpm --force --nodeps --install ${releaseURL}${releaseRPM}
+rpm --force --nodeps --install ${epelURL}${epelRPM}
 rpm --rebuilddb
 "
 
@@ -49,25 +66,9 @@ pkg_mgr groupinstall 'Development Tools'
 touch ${chroot}/etc/sysconfig/network # must be present for network to be configured
 
 # readahead-collector was pegging CPU on startup
-
 echo 'READAHEAD_COLLECT="no"' >> ${chroot}/etc/sysconfig/readahead
 echo 'READAHEAD_COLLECT_ON_RPM="no"' >> ${chroot}/etc/sysconfig/readahead
 
-# Setting timezone
-cp ${chroot}/usr/share/zoneinfo/UTC ${chroot}/etc/localtime
+cp ${chroot}/usr/share/zoneinfo/UTC ${chroot}/etc/localtime # Timezone
+echo "LANG=\"en_US.UTF-8\"" >> ${chroot}/${locale_file}     # Locale
 
-# Setting locale
-case "${stemcell_operating_system_version}" in
-  "6.5")
-    locale_file=/etc/sysconfig/i18n
-    ;;
-  "7")
-    locale_file=/etc/locale.conf
-    ;;
-  *)
-    echo "Unknown CentOS release: ${stemcell_operating_system_version}"
-    exit 1
-    ;;
-esac
-
-echo "LANG=\"en_US.UTF-8\"" >> ${chroot}/${locale_file}
--- stemcell_builder/stages/base_centos_packages/apply.sh
+++ stemcell_builder/stages/base_centos_packages/apply.sh
@@ -3,47 +3,44 @@
 set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
+
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/etc/settings.bash
 
 case "${stemcell_operating_system_version}" in
-  "6.5")
+  (6|6.*) releaseVersion=6
     init_package_name="upstart"
     version_specific_packages="nc"
     ;;
-  "7")
+  (7|7.*) 
     init_package_name="systemd"
-    version_specific_packages="rsyslog rsyslog-relp rsyslog-gnutls rsyslog-mmjsonparse"
-
-    # note about dip group: it has been removed in CentOS 7, but the os-independent stuff elsewhere
-    # in stemcell builder assumes the group exists. So we create it here.
-    if ! grep -q dip $chroot/etc/group; then
-      run_in_chroot $chroot "groupadd -g 40 dip"
-    fi
+    version_specific_packages="nmap-ncat rsyslog rsyslog-relp rsyslog-gnutls rsyslog-mmjsonparse"
+    # note about dip group: it has been removed in CentOS 7, but the 
+    # os-independent stuff elsewhere in stemcell builder assumes the group 
+    # exists. So we create it here.
+    run_in_chroot $chroot "groupadd -g 40 dip"
     ;;
-  *)
+  (*)
     echo "Unknown centos version: ${stemcell_operating_system_version}"
     exit 1
     ;;
 esac
 
-# The CentOS 6.5 script upgraded upstart first, "to prevent it from messing up our stubs and starting daemons anyway"
-# so we'll upgrade systemd for possibly the same reason
+# The CentOS 6.5 script upgraded upstart first,
+# "to prevent it from messing up our stubs and starting daemons anyway"
+# so we'll upgrade systemd for possibly the same reason...
 pkg_mgr install ${init_package_name}
 
 # Install base packages needed by both the warden and bosh
-packages="openssl-devel lsof \
-strace bind9-host dnsutils tcpdump iputils-arping \
-curl wget libcurl3 libcurl3-dev bison \
-readline-devel \
-libxml2 libxml2-devel libxslt libxslt-devel \
-dhclient \
-zip unzip \
-nfs-common flex psmisc apparmor-utils iptables sysstat \
-rsync openssh-server traceroute libncurses5-dev quota \
-libaio1 gdb libcap2-bin libcap-devel bzip2-devel \
-cmake sudo libuuid-devel parted NetworkManager e2fsprogs"
-pkg_mgr install ${packages} ${version_specific_packages}
+packages=(
+openssl-devel lsof strace bind9-host dnsutils tcpdump iputils-arping curl wget 
+libcurl3 libcurl3-dev bison readline-devel libxml2 libxml2-devel libxslt 
+libxslt-devel dhclient zip unzip nfs-common flex psmisc apparmor-utils iptables 
+sysstat rsync openssh-server traceroute libncurses5-dev quota libaio1 gdb 
+libcap2-bin libcap-devel bzip2-devel cmake sudo libuuid-devel parted 
+NetworkManager e2fsprogs
+)
+pkg_mgr install ${packages[@]} ${version_specific_packages}
 
 # Install runit
 pkg_mgr install "rpm-build rpmdevtools glibc-static"
@@ -62,7 +59,8 @@ run_in_chroot $chroot "
 run_in_chroot ${chroot} "yum erase firewalld --assumeyes"
 
 # arrange for runit to start when the system boots
-if [ "${init_package_name}" == "systemd" ]; then
+if [[ "${init_package_name}" == "systemd" ]]
+then
   cp $(dirname $0)/assets/runit.service ${chroot}/usr/lib/systemd/system/
   run_in_chroot ${chroot} "systemctl enable runit"
   run_in_chroot ${chroot} "systemctl enable NetworkManager"
